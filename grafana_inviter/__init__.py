e4dc3a17d637fb183aaf630120594e242b4eec88
Martin Migasiewicz <sowasdummes@gmail.com>
2019-03-20 13:07:19 +0100

    Feature/cosmetic improvements (#8)
    * Changed email
    * Define long description content type

--- grafana_inviter/__init__.py
+++ grafana_inviter/__init__.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 __version__ = "0.0.1-dev"
 __author__ = "Martin Migasiewicz"
-__author_mail__ = "martin.sm@web.de"
+__author_mail__ = "martin dot sm at web dot de"
 __copyright__ = "%s, TomTom International B.V." % datetime.now().year
 __description__ = "Python module and script to invite people fetched from LDAP to Grafana"
 __license__ = "Apache 2.0"
