3ea982df30a66db31b24ca6c2fafdb7ebf9d5b1d
tt-ci <noreply@tomtom.com>
2019-04-02 08:04:00 +0000

    Bump version: 0.0.7-dev -> 0.0.7 ***NO_CI***

--- grafana_inviter/__init__.py
+++ grafana_inviter/__init__.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-__version__ = "0.0.7-dev"
+__version__ = "0.0.7"
 __author__ = "Martin Migasiewicz"
 __author_mail__ = "migasiew.nk@gmail.com"
 __copyright__ = "%s, TomTom International B.V." % datetime.now().year
